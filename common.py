a9278a55660755408de5491cb8091acff04051c2
kbpk <jakub.piasecki99@gmail.com>
2021-06-06 15:14:23 +0200

    Add entropy calc

--- common.py
+++ common.py
@@ -1,6 +1,9 @@
 from collections import namedtuple
 from typing import List
 
+import numpy as np
+from scipy.stats import entropy
+
 FrameSize = namedtuple('FrameSize', ['width', 'height'])
 FaultBoundary = namedtuple('FaultBoundary', ['lower', 'upper'])
 
@@ -13,3 +16,9 @@ def bit_list_to_bit_string(bit_list: List[int]) -> str:
 def split_bit_string_to_n_bit_nums(n: int, bit_string: str) -> List[int]:
     n_bit_nums = [int(bit_string[i:i + n], 2) for i in range(0, len(bit_string), n)]
     return n_bit_nums
+
+
+def calc_entropy(nums: List[int]) -> float:
+    p, _ = np.histogram(nums, bins=256, range=(0, 255), density=True)
+    entropy_val = entropy(p, base=2)
+    return entropy_val
